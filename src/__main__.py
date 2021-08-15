eb0e45fdc1351afc510cd91c29834ed60f9a3db0
rbuerki <r2d4@bluewin.ch>
2021-08-15 14:33:14 +0200

    add first proper functions

--- src/__main__.py
+++ src/__main__.py
@@ -1,49 +1,56 @@
+import logging
 from datetime import datetime
 from pathlib import Path
-from typing import List
+from typing import List, SupportsRound
 
+from rich.console import Console
+from rich.logging import RichHandler
 
 import utils
+import foos
+
+console = Console()
 
 CONFIG_PATH = Path.cwd() / "config.yaml"
 
-source_path = Path(utils.read_yaml(CONFIG_PATH, "SOURCE_PATH"))
-target_path = Path(utils.read_yaml(CONFIG_PATH, "TARGET_PATH"))
-sync_dirs = utils.read_yaml(CONFIG_PATH, "SYNC_DIRECTORIES")
+
+def initialize_logger():
+    """Initialize logging to console using rich."""
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    # Create a formatter
+    cformatter = logging.Formatter(fmt="%(message)s", datefmt="[%X]")
+    # Create console handler
+    sh = RichHandler(show_time=False, show_path=False, markup=True)
+    sh.setLevel(logging.DEBUG)
+    sh.setFormatter(cformatter)
+    logger.addHandler(sh)
+
+    return logger
 
 
-def find_last_modified_file(sync_dir_path):
-    """TODO: just for testing. Rewrite to return_last_modification_date()"""
-    time, file_path = max(
-        (f.stat().st_mtime, f) for f in sync_dir_path.rglob("*")
-    )
-    print(datetime.fromtimestamp(time), file_path)
+# TEST
+def main(logger: logging.Logger):
+
+    source: Path = Path(utils.read_yaml(CONFIG_PATH, "SOURCE_PATH"))
+    target: Path = Path(utils.read_yaml(CONFIG_PATH, "TARGET_PATH"))
+    sync_dirs: List[str] = utils.read_yaml(CONFIG_PATH, "SYNC_DIRECTORIES")
+
+    logger.info("\n[bold DARK_MAGENTA]STARTING SYNC PROCESS ...[/]",)
+
+    for sync_dir in [sync_dirs[0]]:  # TODO testing
+        source_dict, target_dict = foos.create_file_dicts(
+            source, source, sync_dir
+        )
+        added, removed, updated = foos.compare_dicts(source_dict, target_dict)
+
+        print(added, removed, updated)
+
+        # sync_dir_path = source / dir
+        # if sync_dir_path.exists():
+        #     foos.find_last_modified_file(sync_dir_path)
 
 
-def list_newly_modified_objects(sync_dir_path: Path, last_modification_date: datetime) -> List[Path]:
-    """TODO"""
-    pass
-
-
-def copy_objects_to_target(sync_dir_path: Path, ...) -> int:
-    """TODO"""
-    counter = 0
-    return counter
-
-
-def find_removed_objects(sync_dir_path: Path, ...) -> List[Path]:
-    """TODO"""
-    pass
-
-
-def delete_removed_objects_on_target():
-    """TODO"""
-    pass
-
-
-# SMALL TEST
-
-for dir in sync_dirs:
-    sync_dir_path = source_path / dir
-    if sync_dir_path.exists():
-        find_last_modified_file(sync_dir_path)
+if __name__ == "__main__":
+    logger = initialize_logger()
+    main(logger)
---
+++ src/foos.py
@@ -0,0 +1,95 @@
+import logging
+from datetime import datetime
+from pathlib import Path
+from typing import Dict, List, Optional, Tuple, Union
+
+logger = logging.getLogger(__name__)
+
+FileDict = Dict[Path, datetime]
+
+
+def find_last_modified_file(sync_dir_path):
+    """TODO: just for testing. Rewrite to return_last_modification_date()"""
+    time, file_path = max(
+        (f.stat().st_mtime, f) for f in sync_dir_path.rglob("*")
+    )
+    print(datetime.fromtimestamp(time), file_path)
+
+
+def create_file_dicts(
+    source: Path, target: Path, sync_dir: str
+) -> Tuple[FileDict, FileDict]:
+    """Create and return a new dict containing all file objects in
+    the sync_dir on source and target, with the relative path as key
+    and the timestamp of the last modification as value.
+    """
+    file_dicts = []
+    for device in [source, target]:
+        file_dict = {
+            f.relative_to(device / sync_dir): f.stat().st_mtime
+            for f in (device / sync_dir).rglob("*")
+            if f.is_file()
+        }
+        # logging.debug(f"{device}, {sync_dir}, {len(file_dict.items())}")  # TODO remove
+        file_dicts.append(file_dict)
+
+    return file_dicts[0], file_dicts[1]
+
+
+def compare_dicts(
+    source_dict: FileDict, target_dict: FileDict,
+) -> Tuple[List[Optional[Path]], List[Optional[Path]], List[Optional[Path]]]:
+    """Compare file dicts for and return a list each for all newly
+    added, updated or deleted filepaths.
+    """
+    if source_dict == target_dict:
+        logger.info(
+            f"No changes detected between target and source. "
+            f"Terminating sync process for directory {sync_dir}."
+        )
+        added = removed = updated = []
+        return added, removed, updated
+
+    else:
+        new_keys = set(source_dict.keys())
+        old_keys = set(target_dict.keys())
+        intersect_keys = new_keys.intersection(old_keys)
+        added = list(new_keys - old_keys)
+        removed = list(old_keys - new_keys)
+        updated = [f for f in intersect_keys if source_dict[f] > target_dict[f]]
+
+        # # Pretify the output if len of object is 0
+        # for iterable in [added, removed, updated]:
+        #     if len(iterable) == 0:
+        #         iterable = "-"
+        #     else:
+        #         iterable = ", ".join([f.name for f in iterable])
+
+        logger.warning(
+            "[dark_red]CHANGES DETECTED in tables and / or views since last run[/]:\n"
+            f"- Added files: {added if len(added) > 0 else '-'}\n"
+            f"- Removed files: {removed if len(removed) > 0 else '-'}\n"
+            f"- Updated files: {updated if len(updated) > 0 else '-'}."
+        )
+        return added, removed, updated
+
+
+# def list_newly_modified_objects(sync_dir_path: Path, last_modification_date: datetime) -> List[Path]:
+#     """TODO"""
+#     pass
+
+
+# def copy_objects_to_target(sync_dir_path: Path, ...) -> int:
+#     """TODO"""
+#     counter = 0
+#     return counter
+
+
+# def find_removed_objects(sync_dir_path: Path, ...) -> List[Path]:
+#     """TODO"""
+#     pass
+
+
+# def delete_removed_objects_on_target():
+#     """TODO"""
+#     pass

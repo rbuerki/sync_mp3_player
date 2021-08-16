17bfc66aeecaab56651f22d2e6db4aa432c8ca81
rbuerki <r2d4@bluewin.ch>
2021-08-16 21:53:14 +0200

    Set up a little test ...

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,25 @@ import pytest
 
 # Append source code path to sys.path()
 sys.path.append(str(Path.cwd().parent / "src"))
+
+
+@pytest.fixture
+def source_dict():
+    """This is the first base df for testing."""
+    return {
+        "unchanged_1": 5,
+        "unchanged_2": 7,
+        "new": 3,
+        "updated": 4,
+    }
+
+
+@pytest.fixture
+def target_dict():
+    """This is the first base df for testing."""
+    return {
+        "unchanged_1": 5,
+        "unchanged_2": 7,
+        "updated": 3,
+        "deleted": 9,
+    }
---
+++ tests/test_foos.py
@@ -0,0 +1,15 @@
+from src import foos
+
+
+def test_compare_dicts_1(source_dict, target_dict):
+    result = foos.compare_dicts(source_dict, source_dict, "test")
+    assert result[0] == result[1] == result[2] == []
+
+
+def test_compare_dicts_2(source_dict, target_dict):
+    result = foos.compare_dicts(source_dict, target_dict, "test")
+    assert (
+        result[0] == ["new"]
+        and result[1] == ["updated"]
+        and result[2] == ["deleted"]
+    )
